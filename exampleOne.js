932217f4942e168024f228f9fa3d2dac5f24594f
dariaduda <96076657+dariaduda@users.noreply.github.com>
2021-12-26 22:30:45 +0300

    Initial commit

---
+++ exampleOne.js
@@ -0,0 +1,9 @@
+const sequenceSum = (begin, end) => {
+  if (begin > end) {
+    return NaN
+  } else if (begin === end) {
+    return begin
+  } else {
+    return begin + sequenceSum(begin + 1, end)
+  }
+};
