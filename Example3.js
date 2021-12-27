e7b9a887a26dea9d0a867cba4dc0aaeaaa83ef89
dariaduda <96076657+dariaduda@users.noreply.github.com>
2021-12-28 00:03:37 +0300

    add file Example3_(imperative iterative process)

---
+++ Example3.js
@@ -0,0 +1,14 @@
+const smallestDivisor = (num) => {
+  if (num < 1) {
+    return NaN
+  }
+  let result = 2;
+  while (resulte >= n) {
+    if (num % n !== 0) {
+    }
+    return n;
+  }
+}
+
+
+console.log(smallestDivisor(15))
